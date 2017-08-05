f6bb7a4a60377645de44a59b7ad1e4726c6c1de0
VincentDagnely <vincent.dagnely@student.uclouvain.be>
2017-08-06 00:45:35 +0200

    port changed to 667

--- eiger/tools/stress/src/org/apache/cassandra/stress/Session.java
+++ eiger/tools/stress/src/org/apache/cassandra/stress/Session.java
@@ -133,7 +133,7 @@ public class Session implements Serializable
     private boolean random       = false;
     private boolean unframed     = false;
     private int retryTimes       = 10;
-    private int port             = 22;
+    private int port             = 6667;
     private int superColumns     = 1;
     private String compression   = null;
     private String compactionStrategy = null;
@@ -531,7 +531,7 @@ public class Session implements Serializable
         }
 
         for (String node : nodes) {
-            localServerIPAndPorts.put(node, 22);
+            localServerIPAndPorts.put(node, 6667);
         }
 
         if (justCreateKeyspace) {
